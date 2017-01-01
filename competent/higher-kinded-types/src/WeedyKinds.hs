5901bde19ec3e44919ae8ac0f755883f2dfa3526
capitanbatata <damian.nadales@gmail.com>
2017-01-01 12:44:05 +0100

    Solved the problem with poly-kinds.

--- competent/higher-kinded-types/src/WeedyKinds.hs
+++ competent/higher-kinded-types/src/WeedyKinds.hs
@@ -1,12 +1,29 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE PolyKinds      #-}
 -- | An illustration of a problem with kinds.
 
 module WeedyKinds where
 
-data T f a = MkT (f a)
+-- | Without having poly-kinds and kind signatures we cannot define type @T0@
+-- below:
+--
+-- > data T f a = MkT (f a)
+-- > type T0 = T Maybe Int
+--
+-- > data F f = MkF (f Int)
+--
+-- > type T1 = T F Maybe
+--
+-- Using @PolyKinds@ solves the problem above.
+
+data T (f :: k -> *) a = MkT (f a)
 type T0 = T Maybe Int
 
 data F f = MkF (f Int)
--- | We cannot define this type
---
--- > type T1 = T F Maybe
+type T1 = T F Maybe
 
+e0 :: T0
+e0 = MkT (Just 0)
+
+e1 :: T1
+e1 = MkT (MkF (Just 0))
